9a99e6ad75b043eb518c24729103b5486f82d52a
SnapolesIvan <144388463+SnapolesIvan@users.noreply.github.com>
2025-04-10 10:57:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Funciones para los registros
+function consultarRegistros() {
+    alert('Consultando todos los registros...');
+    // Implementa la lógica aquí
+  }
+  
+  function consultarIndividual() {
+    alert('Consultando registros de manera individual...');
+    // Implementa la lógica aquí
+  }
+  
+  function editarRegistro() {
+    alert('Editando registros...');
+    // Implementa la lógica aquí
+  }
+  
+  function eliminarRegistro() {
+    alert('Eliminando registros...');
+    // Implementa la lógica aquí
+  }
+  
+  function agregarRegistro() {
+    alert('Agregando registros...');
+    // Implementa la lógica aquí
+  }
